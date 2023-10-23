2fd91a92c8f94159d9d8dea323d16e0de3c045df
Le Anh Toan <toan.la@decentralab.asia>
2023-10-23 10:22:48 +0000

    Merged in hotfix-backtest (pull request #381)
    fix backtest
    
    * fix backtest
    
    
    Approved-by: Tung Le
    Approved-by: DuyenTB
    Approved-by: An Doan

--- src/components/BacktestModal/helper.ts
+++ src/components/BacktestModal/helper.ts
@@ -6,68 +6,75 @@ export function stringifyRequestData(
   data: RequestBackTestData & { testingType: CopyTradeTypeEnum },
   protocol: ProtocolEnum
 ) {
-  const tokenStringifyMapping: Record<string, string> = getTokenTradeList(protocol).reduce((result, token, index) => {
-    return { ...result, [token.address]: index }
-  }, {})
+  try {
+    const tokenStringifyMapping: Record<string, string> = getTokenTradeList(protocol).reduce((result, token, index) => {
+      return { ...result, [token.address]: index }
+    }, {})
 
-  return `${data.accounts[0]}__${data.balance}__${data.orderVolume}__${data.leverage}__${data.fromTime}__${
-    data.toTime
-  }__${data.testingType}__${data.lookBackOrders ? data.lookBackOrders : '0'}__${
-    data.stopLossAmount ? data.stopLossAmount : '0'
-  }__${data.maxVolMultiplier}__${data.reverseCopy ? '1' : '0'}${
-    data.tokenAddresses?.length
-      ? `__${data.tokenAddresses.map((address) => tokenStringifyMapping[address]).join('_')}`
-      : ''
-  }`
+    return `${data.accounts[0]}__${data.balance}__${data.orderVolume}__${data.leverage}__${data.fromTime}__${
+      data.toTime
+    }__${data.testingType}__${data.lookBackOrders ? data.lookBackOrders : '0'}__${
+      data.stopLossAmount ? data.stopLossAmount : '0'
+    }__${data.maxVolMultiplier ? data.maxVolMultiplier : '0'}__${data.reverseCopy ? '1' : '0'}${
+      data.tokenAddresses?.length
+        ? `__${data.tokenAddresses.map((address) => tokenStringifyMapping[address]).join('_')}`
+        : ''
+    }`
+  } catch {
+    return ''
+  }
 }
 export function parseRequestData(params: string | undefined, protocol: ProtocolEnum) {
-  if (!params) return {} as RequestBackTestData & { testingType: CopyTradeTypeEnum }
+  let result = {} as RequestBackTestData & { testingType: CopyTradeTypeEnum }
+  if (!params) return result
   const tokenParseMapping: Record<string, string> = getTokenTradeList(protocol).reduce((result, token, index) => {
     return { ...result, [index]: token.address }
   }, {})
 
   const listData = params.split('__')
+  if (listData.length < 12) return result
 
-  const account = listData[0]
-  const balance = Number(listData[1])
-  const orderVolume = Number(listData[2])
-  const leverage = Number(listData[3])
-  const fromTime = Number(listData[4])
-  const toTime = Number(listData[5])
-  const testingType = listData[6] as CopyTradeTypeEnum
-  const lookBackOrders = Number(listData[7])
-  const stopLossAmount = Number(listData[8])
-  const maxVolMultiplier = Number(listData[9])
-  const reverseCopy = listData[10]
-  const tokenAddresses = listData[11] ? listData[11].split('_').map((index: string) => tokenParseMapping[index]) : []
+  try {
+    const account = listData[0]
+    const balance = Number(listData[1])
+    const orderVolume = Number(listData[2])
+    const leverage = Number(listData[3])
+    const fromTime = Number(listData[4])
+    const toTime = Number(listData[5])
+    const testingType = listData[6] as CopyTradeTypeEnum
+    const lookBackOrders = Number(listData[7])
+    const stopLossAmount = Number(listData[8])
+    const maxVolMultiplier = Number(listData[9])
+    const reverseCopy = listData[10]
+    const tokenAddresses = listData[11] ? listData[11].split('_').map((index: string) => tokenParseMapping[index]) : []
 
-  const parseData = [account, balance, orderVolume, leverage, fromTime, toTime]
+    const parseData = [account, balance, orderVolume, leverage, fromTime, toTime]
 
-  if (parseData.some((data) => !data || isNaN(data as number)) || !tokenAddresses?.length)
-    return {} as RequestBackTestData & { testingType: CopyTradeTypeEnum }
+    if (parseData.some((data) => !data || isNaN(data as number)) || !tokenAddresses?.length) return result
 
-  const result: RequestBackTestData & { testingType: CopyTradeTypeEnum } = {
-    accounts: [account],
-    balance,
-    orderVolume,
-    leverage,
-    fromTime,
-    toTime,
-    testingType,
-    tokenAddresses,
-  }
-  if (!isNaN(lookBackOrders) && lookBackOrders > 0) {
-    result.volumeProtection = true
-    result.lookBackOrders = lookBackOrders
-  }
-  if (!isNaN(stopLossAmount) && stopLossAmount > 0) {
-    result.enableStopLoss = true
-    result.stopLossAmount = stopLossAmount
-  }
-  if (!isNaN(maxVolMultiplier)) {
-    result.maxVolMultiplier = maxVolMultiplier
-  }
-  if (reverseCopy === '1') result.reverseCopy = true
+    result = {
+      accounts: [account],
+      balance,
+      orderVolume,
+      leverage,
+      fromTime,
+      toTime,
+      testingType,
+      tokenAddresses,
+    }
+    if (!isNaN(lookBackOrders) && lookBackOrders > 0) {
+      result.volumeProtection = true
+      result.lookBackOrders = lookBackOrders
+    }
+    if (!isNaN(stopLossAmount) && stopLossAmount > 0) {
+      result.enableStopLoss = true
+      result.stopLossAmount = stopLossAmount
+    }
+    if (!isNaN(maxVolMultiplier)) {
+      result.maxVolMultiplier = maxVolMultiplier
+    }
+    if (reverseCopy === '1') result.reverseCopy = true
+  } catch {}
   return result
 }
 
