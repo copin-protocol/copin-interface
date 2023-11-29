148190bc3cc1f5d476252f50701b15aadbc38f8c
An Doan <andoan@decentralab.asia>
2023-11-29 15:53:36 +0000

    Merged in hotfix/all-time-when-refresh (pull request #484)
    depend profile
    
    * depend profile
    
    
    Approved-by: Le Anh Toan
    Approved-by: DuyenTB

--- src/pages/Home/ListTradersSection/useTimeFilterData.ts
+++ src/pages/Home/ListTradersSection/useTimeFilterData.ts
@@ -3,7 +3,8 @@ import { useQuery } from 'react-query'
 import { getTradersApi } from 'apis/traderApis'
 import { RequestBodyApiData } from 'apis/types'
 import { TimeFilterProps } from 'components/@ui/TimeFilter'
-import { ProtocolEnum } from 'utils/config/enums'
+import useMyProfileStore from 'hooks/store/useMyProfile'
+import { ProtocolEnum, SubscriptionPlanEnum, TimeFilterByEnum } from 'utils/config/enums'
 import { QUERY_KEYS } from 'utils/config/keys'
 
 const useTimeFilterData = ({
@@ -19,6 +20,7 @@ const useTimeFilterData = ({
   timeOption: TimeFilterProps
   isRangeSelection: boolean
 }) => {
+  const { myProfile } = useMyProfileStore()
   const { data: timeTraders, isFetching: loadingTimeTraders } = useQuery(
     [QUERY_KEYS.GET_TOP_TRADERS, timeOption.id, requestData, tab, protocol],
     () =>
@@ -29,7 +31,14 @@ const useTimeFilterData = ({
           queries: [...(requestData.queries ?? []), { fieldName: 'type', value: timeOption.id }],
         },
       }),
-    { keepPreviousData: true, retry: 0, enabled: !!timeOption && !isRangeSelection }
+    {
+      keepPreviousData: true,
+      retry: 0,
+      enabled:
+        !!timeOption &&
+        !isRangeSelection &&
+        (timeOption.id !== TimeFilterByEnum.ALL_TIME || myProfile?.plan === SubscriptionPlanEnum.PREMIUM),
+    }
   )
   return { timeTraders, loadingTimeTraders }
 }
