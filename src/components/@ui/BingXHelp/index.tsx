d991011932f7283147eedfca25db9af2ebeb34e9
Le Anh Toan <toan.la@decentralab.asia>
2024-01-30 04:05:23 +0000

    Merged in COP-669-add-bingx-ref-note-on-page (pull request #632)
    COP-669 add bingx ref note on page
    
    * init
    
    * change logic show banner
    
    
    Approved-by: DuyenTB
    Approved-by: An Doan

--- src/components/@ui/BingXHelp/index.tsx
+++ src/components/@ui/BingXHelp/index.tsx
@@ -1,61 +1,101 @@
 import { Trans } from '@lingui/macro'
 import { ArrowSquareOut } from '@phosphor-icons/react'
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import { Button } from 'theme/Buttons'
 import ButtonWithIcon from 'theme/Buttons/ButtonWithIcon'
-import { Flex, Type } from 'theme/base'
+import CopyButton from 'theme/Buttons/CopyButton'
+import { Box, Flex, Li, Type } from 'theme/base'
 import { BoxProps } from 'theme/types'
 import { LINKS } from 'utils/config/constants'
 
-const BingXHelp = ({ ...props }: BoxProps) => {
+const BingXHelp = ({
+  hasBorder = false,
+  isList = false,
+  ...props
+}: BoxProps & { hasBorder?: boolean; isList?: boolean }) => {
   const handleOpenUrl = (url: string) => {
     return window.open(url, '_blank')
   }
+  const Wrapper = isList ? Li : Fragment
 
   return (
     <Flex flexDirection="column" sx={{ gap: 20 }} {...props}>
-      <Flex alignItems="center" sx={{ gap: 2 }}>
-        <Type.Caption>
-          <Trans>Don’t have a BingX account?</Trans>
-        </Type.Caption>
-        <Button
-          type="button"
-          variant="ghostPrimary"
-          onClick={() => handleOpenUrl(LINKS.registerBingX)}
-          sx={{ mx: 0, p: 0 }}
+      <Wrapper>
+        <Box
+          sx={{
+            ...(hasBorder
+              ? { p: 2, border: 'small', bg: 'neutral6', borderColor: 'neutral4', borderRadius: 'sm' }
+              : {}),
+          }}
         >
-          <Trans>Register</Trans>
-        </Button>
-      </Flex>
+          <Flex mb={2} sx={{ alignItems: 'center', gap: 2, justifyContent: 'space-between' }}>
+            <Type.Caption>
+              <Trans>Don’t have a BingX account?</Trans>
+            </Type.Caption>
+            <Button
+              type="button"
+              variant="ghostPrimary"
+              onClick={() => handleOpenUrl(LINKS.registerBingX)}
+              sx={{ mx: 0, p: 0 }}
+            >
+              <Trans>Register</Trans>
+            </Button>
+          </Flex>
+          <Type.Caption color="neutral3">
+            <Trans>Get 10% transaction fee rebate with code:</Trans>{' '}
+            <CopyButton
+              value={'DY5QNN'}
+              sx={{
+                display: 'inline-block',
+                color: 'neutral1',
+                '&:hover': {
+                  color: 'neutral2',
+                  '.icon_wrapper': { color: 'primary2' },
+                },
+                p: 0,
+                '& *': { fontWeight: 700 },
+                '& .icon_wrapper': { color: 'primary1' },
+              }}
+            >
+              DY5QNN
+            </CopyButton>
+          </Type.Caption>
+          {/* <Type.CaptionBold>DY5QNN</Type.CaptionBold> */}
+        </Box>
+      </Wrapper>
 
-      <Flex alignItems="center" sx={{ gap: 2 }}>
-        <Type.Caption>
-          <Trans>How to get BingX API Key?</Trans>
-        </Type.Caption>
-        <ButtonWithIcon
-          type="button"
-          variant="ghostPrimary"
-          icon={<ArrowSquareOut size={20} />}
-          size={20}
-          onClick={() => handleOpenUrl(LINKS.getBingXAPIKey)}
-          sx={{ mx: 0, p: 0 }}
-        />
-      </Flex>
+      <Wrapper>
+        <Flex alignItems="center" sx={{ gap: 2 }}>
+          <Type.Caption>
+            <Trans>How to get BingX API Key?</Trans>
+          </Type.Caption>
+          <ButtonWithIcon
+            type="button"
+            variant="ghostPrimary"
+            icon={<ArrowSquareOut size={20} />}
+            size={20}
+            onClick={() => handleOpenUrl(LINKS.getBingXAPIKey)}
+            sx={{ mx: 0, p: 0 }}
+          />
+        </Flex>
+      </Wrapper>
 
-      <Flex alignItems="center" sx={{ gap: 2 }}>
-        <Type.Caption>
-          <Trans>How to get BingX Secret Key?</Trans>
-        </Type.Caption>
-        <ButtonWithIcon
-          type="button"
-          variant="ghostPrimary"
-          icon={<ArrowSquareOut size={20} />}
-          size={20}
-          onClick={() => handleOpenUrl(LINKS.getBingXAPIKey)}
-          sx={{ mx: 0, p: 0 }}
-        />
-      </Flex>
+      <Wrapper>
+        <Flex alignItems="center" sx={{ gap: 2 }}>
+          <Type.Caption>
+            <Trans>How to get BingX Secret Key?</Trans>
+          </Type.Caption>
+          <ButtonWithIcon
+            type="button"
+            variant="ghostPrimary"
+            icon={<ArrowSquareOut size={20} />}
+            size={20}
+            onClick={() => handleOpenUrl(LINKS.getBingXAPIKey)}
+            sx={{ mx: 0, p: 0 }}
+          />
+        </Flex>
+      </Wrapper>
     </Flex>
   )
 }
--- src/pages/WalletManagement/YouMightNeed/index.tsx
+++ src/pages/WalletManagement/YouMightNeed/index.tsx
@@ -1,6 +1,5 @@
 import { Trans } from '@lingui/macro'
 import { Info } from '@phosphor-icons/react'
-import React from 'react'
 
 import BingXHelp from 'components/@ui/BingXHelp'
 import SectionTitle from 'components/@ui/SectionTitle'
@@ -10,7 +9,7 @@ export default function YouMightNeed() {
   return (
     <Flex px={24} py={24} flexDirection="column" height="100%">
       <SectionTitle icon={<Info size={24} />} title={<Trans>You Might Need</Trans>} />
-      <BingXHelp mt={2} />
+      <BingXHelp mt={2} isList />
     </Flex>
   )
 }
