bdfc3e1fd190d2ed908cd39a7e81eba62d05af63
DuyenTB <duyentb@decentralab.asia>
2024-06-17 08:59:09 +0000

    Merged in improve/add-watcher-wallet (pull request #853)
    add watcher wallet
    
    * add watcher wallet
    
    * rename relayer wallet
    
    
    Approved-by: Le Anh Toan
    Approved-by: An Doan

--- src/pages/SystemStatus/WalletWatcher.tsx
+++ src/pages/SystemStatus/WalletWatcher.tsx
@@ -6,41 +6,49 @@ import ExplorerLogo from 'components/@ui/ExplorerLogo'
 import { VerticalDivider } from 'components/@ui/Table/renderProps'
 import useTraderBalances from 'hooks/features/useTraderBalances'
 import CopyButton from 'theme/Buttons/CopyButton'
-import { Box, Flex, Type } from 'theme/base'
+import { Flex, Type } from 'theme/base'
 import { ProtocolEnum } from 'utils/config/enums'
 import { PROTOCOL_PROVIDER } from 'utils/config/trades'
 import { addressShorten, formatNumber } from 'utils/helpers/format'
 
-const TREASURY_WALLET = '0x76Ea7951214A0a3b8D132fb7C80c174dbeD62bcf'
+const RELAYER_WALLET_OP = '0x76Ea7951214A0a3b8D132fb7C80c174dbeD62bcf'
+const RELAYER_WALLET_ARB = '0xbd3726fc0B8411869aE8268345cFfF48747F39da'
 export default function WalletWatcher() {
+  return (
+    <Flex flexDirection="column" p={3} sx={{ gap: 3, height: '100%', overflow: 'hidden auto' }}>
+      <WalletItem protocol={ProtocolEnum.KWENTA} account={RELAYER_WALLET_OP} />
+      <WalletItem protocol={ProtocolEnum.GMX} account={RELAYER_WALLET_ARB} />
+    </Flex>
+  )
+}
+
+function WalletItem({ protocol, account }: { protocol: ProtocolEnum; account: string }) {
   const { sm } = useResponsive()
   const { balance } = useTraderBalances({
-    account: TREASURY_WALLET,
-    protocol: ProtocolEnum.KWENTA,
+    account,
+    protocol,
   })
   return (
-    <Box p={3} sx={{ height: '100%', overflow: 'hidden auto' }}>
-      <Flex alignItems="center" sx={{ gap: 3, flexWrap: 'wrap' }}>
-        <Type.Caption color="neutral3">Wallet Address:</Type.Caption>
-        <Flex alignItems="center" sx={{ gap: 2, flexWrap: 'wrap' }}>
-          <Type.Caption>{sm ? TREASURY_WALLET : addressShorten(TREASURY_WALLET)}</Type.Caption>
-          <CopyButton
-            type="button"
-            variant="ghost"
-            value={TREASURY_WALLET}
-            size="sm"
-            sx={{ color: 'neutral3', p: 0 }}
-            iconSize={14}
-          />
-          <ExplorerLogo
-            protocol={ProtocolEnum.KWENTA}
-            explorerUrl={`${PROTOCOL_PROVIDER[ProtocolEnum.KWENTA]?.explorerUrl}/address/${TREASURY_WALLET}`}
-            size={16}
-          />
-        </Flex>
-        <VerticalDivider />
-        <Type.CaptionBold>${formatNumber(balance)}</Type.CaptionBold>
+    <Flex alignItems="center" sx={{ gap: 3, flexWrap: 'wrap' }}>
+      <Type.Caption color="neutral3">Wallet Address:</Type.Caption>
+      <Flex alignItems="center" sx={{ gap: 2, flexWrap: 'wrap' }}>
+        <Type.Caption>{sm ? account : addressShorten(account)}</Type.Caption>
+        <CopyButton
+          type="button"
+          variant="ghost"
+          value={account}
+          size="sm"
+          sx={{ color: 'neutral3', p: 0 }}
+          iconSize={14}
+        />
+        <ExplorerLogo
+          protocol={protocol}
+          explorerUrl={`${PROTOCOL_PROVIDER[protocol]?.explorerUrl}/address/${account}`}
+          size={16}
+        />
       </Flex>
-    </Box>
+      <VerticalDivider />
+      <Type.CaptionBold>${formatNumber(balance)}</Type.CaptionBold>
+    </Flex>
   )
 }
