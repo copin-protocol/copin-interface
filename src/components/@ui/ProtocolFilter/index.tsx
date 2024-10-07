32c89720e6a956e6dfb99065aca20ca8e64a9473
DuyenTB <duyentb@decentralab.asia>
2024-10-07 03:52:14 +0000

    Merged in hotfix/responsive-protocol-picker-on-metamask (pull request #1131)
    Hotfix responsive protocol picker on Metamask
    
    * Hotfix responsive protocol picker on Metamask
    
    
    Approved-by: An Doan
    Approved-by: Le Anh Toan

--- src/components/@ui/ProtocolFilter/index.tsx
+++ src/components/@ui/ProtocolFilter/index.tsx
@@ -33,7 +33,7 @@ export function ProtocolFilter({ menuSx = {}, placement = 'bottomRight', ...prop
         menuSx={{
           width: menuSx.width ? menuSx.width : ['90vw', '95vw', '95vw'],
           maxWidth: menuSx.maxWidth ? menuSx.maxWidth : '900px',
-          maxHeight: '75svh',
+          maxHeight: '70svh',
           py: 2,
           ...menuSx,
         }}
