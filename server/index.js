899d8a80babe3d77a8fa26805f080b5b03ebc567
DuyenTB.Dlab <duyentb@decentralab.asia>
2024-10-13 13:52:27 +0700

    Fix route

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => {
   renderHTML(req, res)
 })
-app.use(express.static(resolve(__dirname, '..', 'build'), { maxAge: '30d' }))
+app.use(express.static(resolve(__dirname, '..', 'build', 'index.html'), { maxAge: '30d' }))
 
 app.get('/fee-rebate', getFeeRebate)
 app.get('/events', getEvents)
