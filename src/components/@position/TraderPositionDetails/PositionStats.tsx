686b1198193d21b7d6d8cf4c63b2eaffb41fb8f4
DuyenTB.Dlab <duyentb@decentralab.asia>
2024-10-21 20:58:25 +0700

    Fix text

--- src/components/@position/TraderPositionDetails/PositionStats.tsx
+++ src/components/@position/TraderPositionDetails/PositionStats.tsx
@@ -10,9 +10,10 @@ import { renderEntry, renderSize, renderSizeOpeningWithPrices } from 'components
 import { PositionData } from 'entities/trader'
 import { UsdPrices, useRealtimeUsdPricesStore } from 'hooks/store/useUsdPrices'
 import { Box, Flex, Type } from 'theme/base'
-import { OrderTypeEnum, PositionStatusEnum, ProtocolEnum } from 'utils/config/enums'
+import { OrderTypeEnum, PositionStatusEnum } from 'utils/config/enums'
 import { formatDuration } from 'utils/helpers/format'
 
+import { FEE_WITH_FUNDING_PROTOCOLS } from '../../../utils/config/constants'
 import SharePosition from './SharePosition'
 
 interface PositionStatsProps {
@@ -92,7 +93,7 @@ const DesktopLayout = ({ data, prices, hasFundingFee, hasLiquidate, isOpening, c
               />
             }
           />
-          {data.protocol === ProtocolEnum.HYPERLIQUID ? (
+          {FEE_WITH_FUNDING_PROTOCOLS.includes(data.protocol) ? (
             <DesktopItemInfo
               label={<Trans>Fees & Funding:</Trans>}
               value={
